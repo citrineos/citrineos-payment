f80bb7379cdb7e8dfa0962774f6f971d004ea21d
Christian Weissmann <christian.weissmann@s44.team>
2025-01-16 11:09:44 +0100

    fix: remove linting from ejected build script as it causes problems of switching between ESM and commonjs and the parsing of the file.

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -49,7 +49,6 @@ const babelRuntimeRegenerator = require.resolve('@babel/runtime/regenerator', {
 const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false';
 
 const emitErrorsAsWarnings = process.env.ESLINT_NO_DEV_ERRORS === 'true';
-const disableESLintPlugin = process.env.DISABLE_ESLINT_PLUGIN === 'true';
 
 const imageInlineSizeLimit = parseInt(
   process.env.IMAGE_INLINE_SIZE_LIMIT || '10000',
@@ -722,23 +721,6 @@ module.exports = function (webpackEnv) {
           logger: {
             infrastructure: 'silent',
           },
-        }),
-      !disableESLintPlugin &&
-        new ESLintPlugin({
-          extensions: ['js', 'mjs', 'jsx', 'ts', 'tsx'],
-          formatter: require.resolve('react-dev-utils/eslintFormatter'),
-          eslintPath: require.resolve('eslint'),
-          failOnError: !(isEnvDevelopment && emitErrorsAsWarnings),
-          context: paths.appSrc,
-          cache: true,
-          cacheLocation: path.resolve(
-            paths.appNodeModules,
-            '.cache/.eslintcache',
-          ),
-          // Remove baseConfig to use your own ESLint configuration
-          // baseConfig: undefined,
-          cwd: paths.appPath,
-          resolvePluginsRelativeTo: __dirname,
         }),
     ].filter(Boolean),
     // Turn off performance processing because we utilize
